1779b19b7d0ac189658f2361a2a3d2d63fd335dc
Jaimevs <jaime.vqzan@gmail.com>
2025-11-17 17:05:13 -0600

    ExamenPractico
    add: Implementacion de tarjetas con datos de la Pleicula

--- lib/presentation/providers/providers.dart
+++ lib/presentation/providers/providers.dart
@@ -5,3 +5,11 @@ export 'movies/loading_progress_provider.dart';
 export 'movies/upcoming_this_month_provider.dart';
 export 'movies/mexican_movies_sorted_provider.dart';
 export 'series/series_providers_export.dart';
+
+
+export 'actors/actors_repository_provider.dart';
+export 'actors/actors_by_movie_provider.dart';
+
+// export 'movies/initial_loading_provider.dart';
+
+//export 'movies/movies_slideshow_provider.dart';
